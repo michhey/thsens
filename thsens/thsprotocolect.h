f677df056d07510d9692c3589f3d4b3656d5a987
Michael Heyer <michael.heyer@techspring.de>
2015-02-06 08:15:41 +0100

    add class for EuroChron/Tchibo protocol

---
+++ thsens/thsprotocolect.h
@@ -0,0 +1,29 @@
+// 2015-02-03 <mhprog@gmx.net> http://opensource.org/licenses/mit-license.php
+
+#ifndef THS_PROTOCOL_ECT
+#define THS_PROTOCOL_ECT
+
+#include "thsprotocol.h"
+
+/*
+    EuroChron/Tchibo weather sensor protocol implementations
+ */
+
+class THSProtocolECT : public THSProtocol {
+  public:
+    THSProtocolECT();
+
+    // encode data to binary representation (string with 0/1 characters as bits)  
+    String encode();
+    
+    uint8_t deviceId;
+    uint8_t channel;
+    boolean batteryOk;
+    boolean manualSend;
+    float temperature;
+    float humidity;
+    
+};
+
+#endif
+
